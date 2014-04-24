df0e0d1b988b0cc0187a5b4e8862c9338ceb1ae2
Alejandro Hernández <picanteverde@gmail.com>
2014-04-24 17:53:31 -0300

    adding collections

--- 2014-04-24/backbone/one/app.js
+++ 2014-04-24/backbone/one/app.js
@@ -1,26 +1,54 @@
 (function(){
 	var ClientNewForm = Backbone.View.extend({
 
-		initialize: function(){
+			initialize: function(options){
+				this.collection = options.collection;
+			},
+			template: _.template('<h1>New Client</h1>Name: <input type="text" class="js-name"/> <br/>LastName:<input type="text" class="js-lastName"><br/><button class="js-btnSave">Save</button>'),
+			events: {
+				"click .js-btnSave": "save"
+			},
+			render: function() {
+				this.$el.html(this.template());
+				return this;
+			},
+			save: function(){
+				var	uiName = this.$el.find(".js-name"),
+					uiLastName = this.$el.find(".js-lastName"),
+					m = new ClientModel({
+					name: uiName.val(),
+					LastName: uiLastName.val(),
+				});
 
-		},
-		template: _.template('<h1>New Client</h1>Name: <input type="text" class="js-name"/> <br/>LastName:<input type="text" class="js-lastName"><br/><button class="js-btnSave">Save</button>'),
-		events: {
-			"click .js-btnSave": "save"
-		},
-		render: function() {
-			this.$el.html(this.template());
-			return this;
-		},
-		save: function(){
-			alert("Trying to Save"); 
-		}
+				this.collection.add(m);
+				uiName.val("");
+				uiLastName.val("");
+			}
 
-	});
+		}),
+		ClientModel = Backbone.Model.extend({
+			validate: function(attr, options){
+				if(!attr.name){
+					alert("Name is empty!");
+				}
+			}
+		}),
+		ClientsCollection = Backbone.Collection.extend({
+			model: ClientModel
+		});
 
-	var a = new ClientNewForm({
-		el: "#app"
-	});
+	
+	function init(){
+		var clients = new ClientsCollection(),
+			a = new ClientNewForm({
+				el: "#app",
+				collection: clients
+			});
+		a.render();
+	}
 
-	a.render();
+
+	init();
+
+	
 }());
