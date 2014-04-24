112d3f438536f230c5e78bc243033b8f2bd300e8
Alejandro Hernández <picanteverde@gmail.com>
2014-04-24 17:34:03 -0300

    first view

--- 2014-04-24/backbone/one/app.js
+++ 2014-04-24/backbone/one/app.js
@@ -1,3 +1,29 @@
 (function(){
-	
+	var MyView = Backbone.View.extend({
+
+		initialize: function(){
+
+		},
+		template: _.template("<h1><%=title%></h1> <button class=\"js-btnSave\">Save</button>"),
+		events: {
+			"click .js-btnSave": "save"
+		},
+		render: function() {
+			this.$el.html(this.template(this.model.attributes));
+			return this;
+		},
+		save: function(){
+			alert("Trying to Save"); 
+		}
+
+	});
+
+	var a = new MyView({
+		el: "#app",
+		model: new Backbone.Model({
+			title: "hola"
+		})
+	});
+
+	a.render();
 }());
